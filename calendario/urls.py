3f3cb7f2b7181de7848dc2ffbf9a7fa17060d208
GabrielDelPino <gabrieldelpino10@gmail.com>
2024-10-06 18:35:57 -0300

    Calendario creado

--- calendario/urls.py
+++ calendario/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.calendar_view, name='calendar'),
+    path('', views.calendar_view, name='calendar'),  # Vista del calendario
+    path('add/', views.event_create, name='event_create'),  # Vista para crear un evento
 ]
+
+
+
 
 
 
--- calendario/views.py
+++ calendario/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
-from .models import Event
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Event
+from .forms import EventForm
 
 @login_required
 def calendar_view(request):
     events = Event.objects.filter(user=request.user)
     return render(request, 'calendario/calendar.html', {'events': events})
+
+@login_required
+def event_create(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.user = request.user  # Asocia el evento al usuario actual
+            event.save()
+            return redirect('calendar')  # Redirige a la vista del calendario
+    else:
+        form = EventForm()
+    return render(request, 'calendario/add_event.html', {'form': form})
+
+
+
 
 
 
